31d23d714451a059eb79405ef354795cba6f2a41
lingadurai03 <lingadurai033@gmail.com>
2025-01-09 11:44:45 +0530

    Add: second largest number

---
+++ Arrays/secondlargestNumber.js
@@ -0,0 +1,37 @@
+// Second Largest
+// Difficulty: EasyAccuracy: 26.72%Submissions: 936K+Points: 2
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+// Note: The second largest element should not be equal to the largest element.
+
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 and the second largest element does not exist.
+// Constraints:
+// 2 ≤ arr.size() ≤ 105
+// 1 ≤ arr[i] ≤ 105
+
+function findSecondLargestNumber(arr) {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+  }
+  return secondMax;
+}
+
+console.log(findSecondLargestNumber([12, 35, 1, 10, 34, 1]));
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# GFG_DSA_160_JS
-This repository contains solutions to GeeksforGeeks' 160 Days DSA Challenge in JavaScript. The goal is to strengthen problem-solving skills by implementing various data structures and algorithms in JavaScript.
+# GFG 160 Days DSA in JavaScript
+
+This repository holds my solutions to the GeeksforGeeks 160 Days DSA Challenge in JavaScript. It covers essential data structures and algorithms, organized by topics.
+
+---
+
+## **Array**
+
+| Date       | Problems Solved                                            |
+| ---------- | ---------------------------------------------------------- |
+| 2025-01-09 | - [Second Largest Number](./Arrays/secondlargestNumber.js) |
