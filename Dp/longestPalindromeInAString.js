c81bd94a4e491934d3063ff09ff8761f89879f95
Lingadurai03 <lingadurai033@gmail.com>
2025-04-22 11:32:02 +0530

    Add: Longest Palindrome in a String

--- Dp/longestPalindromeInAString.js
+++ Dp/longestPalindromeInAString.js
@@ -27,25 +27,68 @@
 // Company Tags
 // ZohoAccoliteAmazonMicrosoftSamsungMakeMyTripVisaWalmartGoogleQualcommGroupon
 
-function solution(str) {
-  let resIdx = 0;
-  let resLen = 0;
-  let n = str.length - 1;
-  let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
+// O(n^2) && O(n^2)
+// function solution(str) {
+//   let resIdx = 0;
+//   let resLen = 0;
+//   let n = str.length - 1;
+//   let dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
 
-  for (let i = n; i >= 0; i--) {
-    for (let j = i; j <= n; j++) {
-      if (str[i] == str[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
-        dp[i][j] = true;
+//   for (let i = n; i >= 0; i--) {
+//     for (let j = i; j <= n; j++) {
+//       if (str[i] == str[j] && (j - i <= 2 || dp[i + 1][j - 1])) {
+//         dp[i][j] = true;
 
-        if (resLen < j - i + 1) {
-          resIdx = i;
-          resLen = j - i + 1;
-        }
+//         if (resLen < j - i + 1) {
+//           resIdx = i;
+//           resLen = j - i + 1;
+//         }
+//       }
+//     }
+//   }
+//   return str.slice(resIdx, resIdx + resLen);
+// }
+
+// console.log(solution('forgeeksskeegfor'));
+
+// optimized
+
+class Solution {
+  longestPalindrome(s) {
+    // code here
+    const p = this.manacher(s);
+    let resLen = 0;
+    let centerIdx = 0;
+    for (let i = 0; i < p.length; i++) {
+      if (p[i] > resLen) {
+        resLen = p[i];
+        centerIdx = i;
       }
     }
+    const resIdx = (centerIdx - resLen) / 2;
+    return s.substring(resIdx, resIdx + resLen);
   }
-  return str.slice(resIdx, resIdx + resLen);
-}
+  manacher(s) {
+    let t = '#' + s.split('').join('#') + '#';
+    let n = t.length;
+    let p = new Array(n).fill(0);
 
-console.log(solution('forgeeksskeegfor'));
+    let l = 0,
+      r = 0;
+    for (let i = 0; i < n; i++) {
+      p[i] = i < r ? Math.min(r - i, p[l + (r - i)]) : 0;
+      while (
+        i + p[i] + 1 < n &&
+        i - p[i] - 1 >= 0 &&
+        t[i + p[i] + 1] == t[i - p[i] - 1]
+      ) {
+        p[i]++;
+      }
+      if (i + p[i] > r) {
+        l = i - p[i];
+        r = i + p[i];
+      }
+    }
+    return p;
+  }
+}
